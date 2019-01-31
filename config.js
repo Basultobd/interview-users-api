7a00ca0b922a9a6b3c42577a22170450625e16b0
Daniel <basultobd@gmail.com>
2019-01-31 17:00:16 -0600

    add salt value to config

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 const config = {
-  app:{
+  app: {
     port: 3000
   },
   version: 'v1',
   jwtSecret: 'PleaseGiveMeTheJob',
-  jwtDuration: '2 hours'
+  jwtDuration: '2 hours',
+  hashSalt: 10
 };
 
 module.exports = config;
