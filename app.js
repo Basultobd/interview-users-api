1f5416addebcca0e07fdfa88038de246d9214822
basultobd <basultobd@gmail.com>
2019-02-08 15:43:44 -0600

    format to ES6

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 // If you don't explicitly name the file you want to
 // require, Node will automatically look for the index.js file
 const routes = require('./routes');
-app.use('/api/' + config.version, routes);
+app.use(`/api/${config.version}`, routes);
 
 app.listen(config.app.port, () => {
   console.log('Coati user server listening on port 3000!');
